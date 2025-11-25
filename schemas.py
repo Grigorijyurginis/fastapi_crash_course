4b51c20ebe423287e31115aedb15fb638b182354
Grigory Yurginis <g.urginis@anviovr.com>
2025-11-25 23:22:57 +0300

    bug fix

--- schemas.py
+++ schemas.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class STaskAdd(BaseModel):
@@ -15,3 +15,5 @@ class STaskAddResponse(BaseModel):
 
 class STask(STaskAdd):
     id: int
+
+    model_config = ConfigDict(from_attributes=True)
